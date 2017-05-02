8ba4fe25aa12b95290ffd022eca35416e96da935
Daniel Phillips <danieledward.phillips@gmail.com>
2017-05-02 09:09:55 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,21 @@
+Says "Hello, This is a Grocery Store!"
+
+gets the response
+  if any lower case letters
+    replies I AM HAVING A HARD TIME HEARING YOU
+  If all letters are in uppercase
+    replies NO, THIS IS NOT A PET STORE
+
+If no content in respose,
+  replies HELLO!?
+If response is GOODBYE
+  reply I AM HAVING A HARD TIME HEARING YOU
+  If response is anything but goodbye
+  reply HELLO?
+  If Respnse is GOODBYE
+  reply THANK YOU FOR CALLING
+
+
+#for tomorrow...
+ready_to_quit = false
+puts
