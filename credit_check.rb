4aad26628526a700c9799d1257c040d0059e2909
Daniel Phillips <danieledward.phillips@gmail.com>
2017-05-05 21:03:30 -0600

    starts project

---
+++ credit_check.rb
@@ -0,0 +1,26 @@
+
+require 'pry'
+card_number = [4, 9, 2, 9, 7, 3, 5, 4, 7, 7, 2, 5, 0, 5, 4, 3]
+
+valid = false
+
+checked_card = []
+
+card_number.each do |x|
+  if card_number[0].odd? == true
+    puts x
+  end
+end
+
+puts card_number
+
+
+
+
+# Your Luhn Algorithm Here
+
+
+
+# Output
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
